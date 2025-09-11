e27fa53701692a85130096e43afb2efb3e8d251a
nikmosi <i@nikmosi.ru>
2025-09-12 02:42:11 +0700

    refactor(rc): add local to globalkeys

--- rc.lua
+++ rc.lua
@@ -324,7 +324,7 @@ root.buttons(gears.table.join(
 -- }}}
 
 -- {{{ Key bindings
-globalkeys = gears.table.join(
+local globalkeys = gears.table.join(
 	awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
 	awful.key({ modkey }, "Left", awful.tag.viewprev, { description = "view previous", group = "tag" }),
 	awful.key({ modkey }, "Right", awful.tag.viewnext, { description = "view next", group = "tag" }),
