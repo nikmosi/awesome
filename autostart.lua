aa2ad368e80451d871ba98bcefe6e1d0cbee7715
nikmosi <i@nikmosi.ru>
2025-09-28 14:20:39 +0700

    feat(autostart)!: add props

--- autostart.lua
+++ autostart.lua
@@ -1,18 +1,85 @@
 local awful = require("awful")
 
-local commands = {
-	"setxkbmap -layout us,ru -option grp:alt_shift_toggle",
-	"chatterino",
-	"hyperhdr --desktop",
-	"ayugram-desktop",
-	"nekoray",
-	"firefox",
-	"easyeffects",
-	"prismatik && sleep 0.5 && prismatik --on && prismatik --on",
+-- общий пресет для всех окон, созданных этими спавнами
+local DEFAULT = {
+	floating = false,
+	focus = false,
+	switch_to_tags = false,
+	honor_padding = true,
+	honor_workarea = true,
 }
 
-for _, cmd in ipairs(commands) do
-	awful.spawn.once(cmd, {})
+local function merge(a, b)
+	local r = {}
+	for k, v in pairs(a or {}) do
+		r[k] = v
+	end
+	for k, v in pairs(b or {}) do
+		r[k] = v
+	end
+	return r
+end
+
+-- список приложений: команда + частные свойства
+local APPS = {
+	{ -- раскладка, окна нет: свойства не важны
+		cmd = "setxkbmap -layout us,ru -option grp:alt_shift_toggle",
+		props = {},
+		shell = false,
+	},
+	{
+		cmd = "chatterino",
+		props = {
+			urgent = false,
+			floating = false,
+		},
+	},
+	{
+		cmd = "hyperhdr --desktop",
+		props = {
+			floating = true,
+			minimized = true,
+		},
+	},
+	{
+		cmd = "ayugram-desktop",
+		props = {
+			urgent = false,
+			floating = false,
+		},
+	},
+	{
+		cmd = "nekoray",
+		props = {
+			floating = true,
+			urgent = false,
+		},
+	},
+	{
+		cmd = "firefox",
+		props = {
+			floating = false,
+		},
+	},
+	{
+		cmd = "easyeffects",
+		props = {
+			urgent = false,
+			floating = true,
+			minimized = true,
+			ontop = false,
+		},
+	},
+}
+
+-- запуск
+for _, app in ipairs(APPS) do
+	local props = merge(DEFAULT, app.props)
+	if app.shell then
+		awful.spawn.with_shell(app.cmd, props) -- свойства пойдут через DESKTOP_STARTUP_ID
+	else
+		awful.spawn(app.cmd, props)
+	end
 end
 
 return {}
